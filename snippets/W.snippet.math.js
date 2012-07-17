8177865ca779849797299a3d844b59c19eaaa2d4
Ross Cairns <ross@electricglen.com>
2012-07-17 16:49:14 +0100

    Fisher-Yates shuffleArray

--- snippets/W.snippet.math.js
+++ snippets/W.snippet.math.js
@@ -79,4 +79,17 @@
         return Math.sqrt((x*x)+(y*y));
     };
 
+    // Fisher-Yates shuffle.
+    // source http://stackoverflow.com/questions/962802/is-it-correct-to-use-javascript-array-sort-method-for-shuffling
+    W.snippet.math.randomizedArray = function (arr, leaveOriginalUntouched) {
+        var array = (leaveOriginalUntouched) ? arr.slice(0) : arr;
+        var tmp, current, top = array.length;
+        if(top) while(--top) {
+            current = Math.floor(Math.random() * (top + 1));
+            tmp = array[current];
+            array[current] = array[top];
+            array[top] = tmp;
+        }
+        return array;
+    };
 }());
