a54c31c4ee327b991fa532434911845af94095a8
Ross Cairns <ross@electricglen.com>
2013-04-11 03:08:45 +0100

    Added hexStringToColorArray

--- W.Math.js
+++ W.Math.js
@@ -106,6 +106,22 @@
         return array;
     };
 
+    W.Math.hexStringToColorArray  = function (hex)  {
+        // modified from http://stackoverflow.com/a/5624139/179015
+        // Expand shorthand form (e.g. "03F") to full form (e.g. "0033FF")
+        var shorthandRegex = /^#?([a-f\d])([a-f\d])([a-f\d])$/i;
+        hex = hex.replace(shorthandRegex, function(m, r, g, b) {
+            return r + r + g + g + b + b;
+        });
+
+        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+        return result ? [
+            parseInt(result[1], 16),
+            parseInt(result[2], 16),
+            parseInt(result[3], 16)
+        ] : [];
+    };
+
     W.Math.colorStringToHex = function (color) {
         if (color.substr(0, 1) === '#') {
             return color;
