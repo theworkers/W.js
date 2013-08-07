2e9e093adab3c83ecd6b0526c238e21211ed926d
Ross Cairns <ross@electricglen.com>
2013-08-07 20:31:37 +0100

    Added Math.clamp callbackOnClamp

--- build/W.js
+++ build/W.js
@@ -1401,19 +1401,31 @@
         return x;
     };
 
-    W.Math.clamp = function (value, min, max) {
+    W.Math.clamp = function (value, min, max, callbackOnClamp) {
         if (max < min) {
             if (value < max) {
+                if (typeof callbackOnClamp === 'function') {
+                    callbackOnClamp(value, max);
+                }
                 value = max;
             }
             else if (value > min) {
+                if (typeof callbackOnClamp === 'function') {
+                    callbackOnClamp(value, min);
+                }
                 value = min;
             }
         } else {
             if (value > max) {
+                if (typeof callbackOnClamp === 'function') {
+                    callbackOnClamp(value, max);
+                }
                 value = max;
             }
             else if (value < min) {
+                if (typeof callbackOnClamp === 'function') {
+                    callbackOnClamp(value, min);
+                }
                 value = min;
             }
         }
