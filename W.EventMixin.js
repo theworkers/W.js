9f240436dcdd7f85d73260cc5125461d6322ab64
Ross Cairns <ross@electricglen.com>
2013-08-19 20:07:22 +0100

    Events return themselves

--- W.EventMixin.js
+++ W.EventMixin.js
@@ -26,6 +26,7 @@
                     this.events()[event].push(callback);
                 }
             }
+            return this;
         },
         /** Remove event listener */
         off : function (/** String */ event, /** Function */ callback) {
@@ -41,6 +42,7 @@
                     }
                 }
             }
+            return this;
         },
         /** Fire the event  */
         trigger : function (event, eventData) {
