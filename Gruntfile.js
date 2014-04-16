3fddf80ccd039e0fc05fb9eb389d9dc2aa278819
Ross Cairns <ross@electricglen.com>
2014-04-16 18:08:08 +0100

    Added bump to grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -139,4 +139,5 @@ module.exports = function( grunt ) {
     grunt.loadNpmTasks( 'grunt-mocha-test' );
     grunt.loadNpmTasks( 'grunt-mocha' );
     grunt.loadNpmTasks( 'grunt-contrib-uglify' );
+    grunt.loadNpmTasks( 'grunt-bump' ); // bump:minor
 };
