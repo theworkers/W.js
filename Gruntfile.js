4082be344ac6cf6561aefdf26fdfbd0cb218f261
Ross Cairns <ross@electricglen.com>
2014-05-13 17:14:25 +0100

    Fix: grunt still pushing

--- Gruntfile.js
+++ Gruntfile.js
@@ -130,6 +130,8 @@ module.exports = function( grunt ) {
                 createTag: true,
                 tagName: 'v%VERSION%',
                 tagMessage: 'Version %VERSION%',
+                push: false,
+                pushTo: 'upstream',
                 gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d' // options to use with '$ git describe'
             }
         }
