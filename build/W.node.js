17cde94b9dc74fb2f4b3a2ef9e2fba9f79fb6602
Ross Cairns <ross@electricglen.com>
2014-06-26 13:51:38 +0100

    Improved docs

--- build/W.node.js
+++ build/W.node.js
@@ -31,11 +31,11 @@ function bind( fn, scope ) {
 }
 // Calls `fn` with any other passed arguments when `fn` is a function (i.e. not defined).
 // For example:
-// function ( callback ) {
-//     var err = null;
-//     var result = 1;
-//     W.call( callback, err, result );
-// }
+//     function ( callback ) {
+//         var err = null;
+//         var result = 1;
+//         W.call( callback, err, result );
+//     }
 //   
 function call ( fn ) {
     if ( typeof fn === 'function' ) {
